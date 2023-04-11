00a1d9cb7a71116d8d284c2a726f3deff380f186
JRYGMS <107040446+JRYGMS@users.noreply.github.com>
2023-04-11 11:57:11 +0200

    Add files via upload

---
+++ zadanie4_Gasior/app/templates_c/f70e3c43ce074c1b92cecd60e3c66480f1cae81e_0.file.main.html.php
@@ -0,0 +1,86 @@
+<?php
+/* Smarty version 4.3.0, created on 2023-04-11 11:37:54
+  from 'C:\xampp\htdocs\zadanie4_Gasior\templates\main.html' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.0',
+  'unifunc' => 'content_64352a72ef0e33_64166674',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'f70e3c43ce074c1b92cecd60e3c66480f1cae81e' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\zadanie4_Gasior\\templates\\main.html',
+      1 => 1681205870,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_64352a72ef0e33_64166674 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+?>
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="<?php echo (($tmp = $_smarty_tpl->tpl_vars['page_description']->value ?? null)===null||$tmp==='' ? 'Opis domyślny' ?? null : $tmp);?>
+">
+	<title><?php echo (($tmp = $_smarty_tpl->tpl_vars['page_title']->value ?? null)===null||$tmp==='' ? "Tytuł domyślny" ?? null : $tmp);?>
+</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container-fluid text-white text-center bg-secondary position-absolute top-0 bottom-0 overflow-auto">
+
+<div class="container">
+	<h1><?php echo (($tmp = $_smarty_tpl->tpl_vars['page_title']->value ?? null)===null||$tmp==='' ? "Tytuł domyślny" ?? null : $tmp);?>
+</h1>
+	<h2><?php echo (($tmp = $_smarty_tpl->tpl_vars['page_header']->value ?? null)===null||$tmp==='' ? "Tytuł domyślny" ?? null : $tmp);?>
+</h1>
+	<p>
+		<?php echo (($tmp = $_smarty_tpl->tpl_vars['page_description']->value ?? null)===null||$tmp==='' ? "Opis domyślny" ?? null : $tmp);?>
+
+	</p>
+</div>
+
+<div class="container">
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_40976557464352a72ef03c0_16844205', 'content');
+?>
+
+</div>
+
+<div class="container p-3">      
+	<p>
+		Widok oparty na frameworku <a href="https://getbootstrap.com/" class="link-warning" target="_blank">Bootstrap</a>.
+	</p>
+</div>
+
+</div>
+<?php echo '<script'; ?>
+ src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+</body>
+</html><?php }
+/* {block 'content'} */
+class Block_40976557464352a72ef03c0_16844205 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'content' => 
+  array (
+    0 => 'Block_40976557464352a72ef03c0_16844205',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+ Domyślna treść zawartości .... <?php
+}
+}
+/* {/block 'content'} */
+}
